01f3dbad7167e39dd230bee0117c74605533d25b
chp <re23m025@technikum-wien.at>
2026-01-06 23:33:38 +0100

    Status: Robot moves around world via Chatbot commands. // Codewise:  Mod: ROS_tools.py -> Added qos, moved tool 'rechner' from previos ROS_cmd_vel_publisher and Chatbot_header_tools // Mod: Chatbot.py -> Updated imports // Del: Chatbot_header_tools, ROS_cmd_vel_publisher

--- ros2_ws/src/turtlebot3_full_bringup/scripts/ROS_tools.py
+++ ros2_ws/src/turtlebot3_full_bringup/scripts/ROS_tools.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 
 import math
 import threading
@@ -14,13 +15,23 @@ class GoalInput(BaseModel):
     x: float = Field(description="X position in map frame")
     y: float = Field(description="Y position in map frame")
     theta: float = Field(description="Yaw angle in radians")
+
 class ROSGoalPublisher(Node):
     def __init__(self):
         super().__init__('llm_goal_publisher')
+
+        qos = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+
         self.publisher = self.create_publisher(
             PoseStamped,
             '/goal_pose',
-            10
+            qos
         )
 
 ros_node = None
@@ -55,4 +66,25 @@ def ROS_send_goal(x: float, y: float, theta: float) -> str:
     ros_node.get_logger().info(f"Goal published: x={x}, y={y}, theha={theta}")
 
     return f"Ziel gesendet: x={x}, y={y}, theta={theta}"
+
+
+@tool
+def rechner(a: int, b: int, operation: str) -> int:
+    """Führt eine mathematische Operation durch.
+    
+    Args:
+        a: Erste Zahl
+        b: Zweite Zahl
+        operation: Die Operation ('add', 'subtract', 'multiply', 'divide')
+    """
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        return a / b if b != 0 else "Division durch Null!"
+    else:
+        return "Unbekannte Operation"
 
