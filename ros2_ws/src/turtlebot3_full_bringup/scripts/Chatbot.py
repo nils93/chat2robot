02b20a8be2974b61738026bfc623c47dd6f5689d
chp <re23m025@technikum-wien.at>
2026-01-19 14:57:29 +0100

    Eliminated tool 'rechner'. Eliminated usage of 'ROS_get_navigation_status' in Chatbot.py

--- ros2_ws/src/turtlebot3_full_bringup/scripts/Chatbot.py
+++ ros2_ws/src/turtlebot3_full_bringup/scripts/Chatbot.py
@@ -11,7 +11,7 @@ from langchain_mistralai import ChatMistralAI
 
 #eigene Module
 from Chatbot_header_RAG import RAG_Functions
-from ROS_tools import ROS_send_goal, rechner, ROS_get_navigation_status
+from ROS_tools import ROS_send_goal
 
 
 def select_llm():
@@ -86,7 +86,7 @@ RAG_1.VectorStorage() #VectorStore aufbauen ->Funktion nochmal selbst machen, ha
 
 
 #Tools einbinden
-tools=[rechner, ROS_send_goal, ROS_get_navigation_status]
+tools=[ROS_send_goal]
 
 # Agent anlegen (gehirn/grundfunktionen)
 LLM_agent= create_tool_calling_agent(LLM, tools, full_prompt) 
